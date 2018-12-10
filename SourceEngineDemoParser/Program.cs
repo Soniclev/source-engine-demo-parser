17173dda0110e8ac08f71fbaed6362b94c72cec3
Victor Kozlov <soniclev9@gmail.com>
2018-12-10 19:26:06 +0300

    Add project files.

---
+++ SourceEngineDemoParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SourceEngineDemoParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
